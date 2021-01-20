c06900c38f2a2792df416a557412f3a9448efdc3
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-01-20 09:05:19 -0500

    updates

--- providers/azurerm/mgtgrp/mgtgrp.tf
+++ providers/azurerm/mgtgrp/mgtgrp.tf
@@ -10,11 +10,11 @@ resource "azurerm_management_group" "example_parent" {
 }
 
 output "mgtgrp_display_name" {
-	value = azurerm_management_group.example_parent.display_name[count.index]
+	value = azurerm_management_group.example_parent.display_name
 }
 output "mgtgrp_name" {
-	value = azurerm_management_group.example_parent.name[count.index]
+	value = azurerm_management_group.example_parent.name
 }
 output "mgtgrp_parent_id" {
-	value = azurerm_management_group.example_parent.parent_management_group_id[count.index]
+	value = azurerm_management_group.example_parent.parent_management_group_id
 }
--- providers/azurerm/rg/rg.tf
+++ providers/azurerm/rg/rg.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "example" {
 }
 
 output "rg_name" {
-	value = azurerm_resource_group.example.name[count.index]
+	value = azurerm_resource_group.example.name
 }
 output "rg_location" {
-	value = azurerm_resource_group.example.location[count.index]
+	value = azurerm_resource_group.example.location
 }
