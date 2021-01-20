43882b5721c8ee5d9ccdcf9a8d82e7ea8f37a4c7
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-01-19 23:15:31 -0500

    updates

--- providers/azurerm/localgw/localgw.tf
+++ providers/azurerm/localgw/localgw.tf
@@ -4,8 +4,8 @@
 
 resource "azurerm_local_network_gateway" "prem" {
   name                = var.localgw_name
-  resource_group_name = var.resource_group
-  location            = var.location
+  resource_group_name = var.rg_name
+  location            = var.rg_location
   gateway_address     = var.gateway_address
   address_space       = var.address_space
 }
