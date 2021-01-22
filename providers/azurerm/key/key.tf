975c5cab50afb5da642105b148bb280bfa89b056
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-01-21 21:28:11 -0500

    added key module

---
+++ providers/azurerm/key/key.tf
@@ -0,0 +1,22 @@
+#================================================================================
+#
+# KEY.TF
+
+resource "azurerm_key_vault_key" "generated" {
+  name         = var.key_name
+  key_vault_id = var.key_id
+  key_type     = var.key_type
+  key_size     = var.key_size
+
+  key_opts = var.key_opts
+}
+
+output "key_id" {
+    value = azurerm_key_vault_key.generated.id
+}
+output "key_name" {
+    value = azurerm_key_vault_key.generated.name
+}
+output "key_version" {
+    value = azurerm_key_vault_key.generated.version
+}
