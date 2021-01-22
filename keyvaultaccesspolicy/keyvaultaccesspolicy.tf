673b9c3081220a7ac7dacec0b99c985e8b451776
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-01-22 09:29:24 -0500

    updated keyvaultaccesspolicy module

--- keyvaultaccesspolicy/keyvaultaccesspolicy.tf
+++ keyvaultaccesspolicy/keyvaultaccesspolicy.tf
@@ -8,12 +8,12 @@ resource "azurerm_key_vault_access_policy" "example" {
     key_vault_id = var.key_vault_id
     tenant_id    = data.azurerm_client_config.current.tenant_id
     object_id    = data.azurerm_client_config.current.object_id
-    key_permissions = var.key_vault_id
+    key_permissions = var.key_permissions
     secret_permissions = var.secret_permissions
     storage_permissions = var.storage_permissions
     certificate_permissions = var.certificate_permissions
 }
 
-output "key_vault_access_policy_id" {
+output "keyvaultaccesspolicy_id" {
     value = azurerm_key_vault_access_policy.example.id
 }
