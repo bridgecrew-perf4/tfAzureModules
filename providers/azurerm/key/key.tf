4d18b54015f8dfb0ca439fccaa83df97f0d329ec
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-01-21 21:43:17 -0500

    update to key module

--- providers/azurerm/key/key.tf
+++ providers/azurerm/key/key.tf
@@ -4,7 +4,7 @@
 
 resource "azurerm_key_vault_key" "generated" {
   name         = var.key_name
-  key_vault_id = var.key_id
+  key_vault_id = var.key_vault_id
   key_type     = var.key_type
   key_size     = var.key_size
 
