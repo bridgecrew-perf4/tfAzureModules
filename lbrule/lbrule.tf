2a8b573feccf0d946b07308dbbec2d5b7f8c210b
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-02-14 17:52:28 -0500

    updates to lb modules

--- lbrule/lbrule.tf
+++ lbrule/lbrule.tf
@@ -1,3 +1,7 @@
+#================================================================================
+#
+# LBRULE.TF
+
 resource "azurerm_lb_rule" "example" {
   resource_group_name            = azurerm_resource_group.example.name
   loadbalancer_id                = azurerm_lb.example.id
