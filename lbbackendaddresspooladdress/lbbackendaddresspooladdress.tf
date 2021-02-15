94890475d863e585d03e159deb9167d53555965d
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-02-14 19:31:02 -0500

    updates to lb modules

--- lbbackendaddresspooladdress/lbbackendaddresspooladdress.tf
+++ lbbackendaddresspooladdress/lbbackendaddresspooladdress.tf
@@ -2,7 +2,7 @@
 #
 # LBBACKENDADDRESSPOOLADDRESS.TF
 
-resource "azurerm_lb_back_end_address_pool_address" "example" {
+resource "azurerm_lb_backend_address_pool_address" "example" {
   name                    = var.lb_backendaddresspooladdress_name
   backend_address_pool_id = var.lb_backendaddresspooladdress_backend_address_pool_id
   virtual_network_id      = var.lb_backendaddresspooladdress_virtual_network_id
@@ -10,8 +10,8 @@ resource "azurerm_lb_back_end_address_pool_address" "example" {
 }
 
 output "lbbackendaddresspooladdress_id" {
-    value = azurerm_lb_back_end_address_pool_address.example.id
+    value = azurerm_lb_backend_address_pool_address.example.id
 }
 output "lbbackendaddresspooladdress_name" {
-    value = azurerm_lb_back_end_address_pool_address.example.name
+    value = azurerm_lb_backend_address_pool_address.example.name
 }
