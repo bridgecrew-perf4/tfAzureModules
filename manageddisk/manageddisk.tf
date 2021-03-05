1f84cab101ddd68012e5276027e71d27bc46398c
C R Cantrell Jr <charles.cantrell@cantrelloffice.cloud>
2021-03-05 16:54:44 -0500

    debugging

--- manageddisk/manageddisk.tf
+++ manageddisk/manageddisk.tf
@@ -12,13 +12,13 @@ resource "azurerm_managed_disk" "example" {
   disk_encryption_set_id = var.disk_encryption_set_id
   disk_iops_read_write   = var.disk_iops_read_write
   disk_mbps_read_write   = var.disk_mbps_read_write
-  encryption_settings    = var.encryption_settings
+  #encryption_settings    = var.encryption_settings
   image_reference_id     = var.image_reference_id
   os_type                = var.os_type
   source_resource_id     = var.source_resource_id
   source_uri             = var.source_uri
   storage_account_id     = var.storage_account_id
-  zones                  = var.zones
+  #zones                  = var.zones
 }
 
 output "manageddisk_id" {
