d1b05500c450d6180c8b775da2d8d4520b64667c
Ron Cantrell <ron.cantrell@cantrelloffice.cloud>
2021-11-21 12:13:08 -0500

    updated

--- vnetgw/vnetgw.tf
+++ vnetgw/vnetgw.tf
@@ -4,27 +4,37 @@
 
 resource "azurerm_virtual_network_gateway" "example" {
   #count = var.to_provision == true ? 1 : 0
-  name                = var.vnetgw_name
-  location            = var.rg_location
-  resource_group_name = var.rg_name
+  vnetgw_location                                         = var.rg_location
+  vnetgw_resource_group_name                              = var.rg_name
+  vnetgw_tags                                             = var.rg_tags
 
-  type     = var.type
-  vpn_type = var.vpn_type
-
-  active_active = var.active_active
-  enable_bgp    = var.enable_bgp
-  sku           = var.sku
+  vnetgw_name                                             = var.vnetgw_name
+  vnetgw_type                                             = var.vnetgw_type
+  vnetgw_vpn_type                                         = var.vnetgw_vpn_type
+  vnetgw_active_active                                    = var.vnetgw_active_active
+  vnetgw_enable_bgp                                       = var.vnetgw_enable_bgp
+  vnetgw_sku                                              = var.vnetgw_sku
 
   ip_configuration {
-    name                          = var.ip_config_name
-    public_ip_address_id          = var.public_ip_address_id
-    private_ip_address_allocation = var.private_ip_address_allocation
-    subnet_id                     = var.subnet_id
+    vnetgw_ip_configuration_name                          = var.vnetgw_ip_configuration_name
+    vnetgw_ip_configuration_public_ip_address_id          = var.vnetgw_ip_configuration_public_ip_address_id
+    vnetgw_ip_configuration_private_ip_address_allocation = var.vnetgw_ip_configuration_private_ip_address_allocation
+    vnetgw_ip_configuration_subnet_id                     = var.vnetgw_ip_configuration_subnet_id
   }
 
   vpn_client_configuration {
-    address_space = var.address_space
-    vpn_client_protocols = var.vpn_client_protocols
+    vnetgw_vpn_client_configuration_address_space        = var.vnetgw_vpn_client_configuration_address_space
+    vnetgw_vpn_client_configuration_vpn_client_protocols = var.vnetgw_vpn_client_configuration_vpn_client_protocols
+    root_certificate {
+      vnetgw_root_certificate_name                       = var.vnetgw_root_certificate_name
+      vnetgw_root_certificate_name_public_cert_data      = var.vnetgw_root_certificate_public_cert_data
+    }
+  }
+  
+  bgp_settings {
+    vnetgw_bgp_settings_asn                              = var.bgp_settings_asn
+    vnetgw_bgp_settings_peering_addresses                = var.bgp_settings_peering_addresses
+    vnetgw_bgp_settings_peer_weight                      = var.bgp_settings_peer_weight
   }
 }
 
--- vnetgw/vnetgw.var.tf
+++ vnetgw/vnetgw.var.tf
@@ -14,55 +14,65 @@ variable "rg_name" {
     type = string
 	default = "rg_name"
 }
+variable "rg_tags" {
+    type = map
+}
 variable "vnetgw_name" {
     type = string
 	default = "vnetgw_name"
 }
-variable "type" {
+variable "vnetgw_type" {
     type = string
     default = "type"
 }
-variable "vpn_type" {
+variable "vnetgw_vpn_type" {
     type = string
     default = "vpn_type"
 }
-variable "active_active" {
+variable "vnetgw_active_active" {
     type = bool
     default = false
 }
-variable "enable_bgp" {
+variable "vnetgw_enable_bgp" {
     type = bool
     default = false
 }
-variable "sku" {
+variable "vnetgw_sku" {
     type = string
     default = "sku"
 }
-variable "ip_config_name" {
+variable "vnetgw_ip_configuration_name" {
     type = string
-    default = "ip_config_name"
+    default = "ip_configuration_name"
 }
-variable "public_ip_address_id" {
+variable "vnetgw_ip_configuration_public_ip_address_id" {
     type = string
     default = "public_ip_address_id"
 }
-variable "private_ip_address_allocation" {
+variable "vnetgw_ip_configuration_private_ip_address_allocation" {
     type = string
     default = "private_ip_address_allocation"
 }
-variable "subnet_id" {
+variable "vnetgw_ip_configuration_subnet_id" {
     type = string
     default = "subnet_id"
 }
-variable "address_space" {
+variable "vnetgw_vpn_client_configuration_address_space" {
     type = list
     default = [
         "10.0.1.0/24"
     ]
 }
-variable "vpn_client_protocols" {
+variable "vnetgw_vpn_client_configuration_vpn_client_protocols" {
     type = list
     default = [
         "vpn_client_protocols"
     ]
+
+variable "vnetgw_root_certificate_name"{}
+variable "vnetgw_root_certificate_public_cert_data"{}
+variable "bgp_settings_asn"{}
+variable "bgp_settings_peering_addresses"{}
+variable "bgp_settings_peer_weight"{}
+
 }
