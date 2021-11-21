4ed4d351d46c93db67ddcb6eab72b14ffd2da605
Ron Cantrell <ron.cantrell@cantrelloffice.cloud>
2021-11-21 12:26:25 -0500

    updated

--- vnetgw/vnetgw.tf
+++ vnetgw/vnetgw.tf
@@ -4,38 +4,41 @@
 
 resource "azurerm_virtual_network_gateway" "example" {
   #count = var.to_provision == true ? 1 : 0
-  vnetgw_location                                         = var.rg_location
-  vnetgw_resource_group_name                              = var.rg_name
-  vnetgw_tags                                             = var.rg_tags
+  location                        = var.rg_location
+  resource_group_name             = var.rg_name
+  tags                            = var.rg_tags
 
-  vnetgw_name                                             = var.vnetgw_name
-  vnetgw_type                                             = var.vnetgw_type
-  vnetgw_vpn_type                                         = var.vnetgw_vpn_type
-  vnetgw_active_active                                    = var.vnetgw_active_active
-  vnetgw_enable_bgp                                       = var.vnetgw_enable_bgp
-  vnetgw_sku                                              = var.vnetgw_sku
+  name                            = var.vnetgw_name
+  type                            = var.vnetgw_type
+  vpn_type                        = var.vnetgw_vpn_type
+  active_active                   = var.vnetgw_active_active
+  enable_bgp                      = var.vnetgw_enable_bgp
+  sku                             = var.vnetgw_sku
 
   ip_configuration {
-    vnetgw_ip_configuration_name                          = var.vnetgw_ip_configuration_name
-    vnetgw_ip_configuration_public_ip_address_id          = var.vnetgw_ip_configuration_public_ip_address_id
-    vnetgw_ip_configuration_private_ip_address_allocation = var.vnetgw_ip_configuration_private_ip_address_allocation
-    vnetgw_ip_configuration_subnet_id                     = var.vnetgw_ip_configuration_subnet_id
+    name                          = var.vnetgw_ip_configuration_name
+    public_ip_address_id          = var.vnetgw_ip_configuration_public_ip_address_id
+    private_ip_address_allocation = var.vnetgw_ip_configuration_private_ip_address_allocation
+    subnet_id                     = var.vnetgw_ip_configuration_subnet_id
   }
 
   vpn_client_configuration {
-    vnetgw_vpn_client_configuration_address_space        = var.vnetgw_vpn_client_configuration_address_space
-    vnetgw_vpn_client_configuration_vpn_client_protocols = var.vnetgw_vpn_client_configuration_vpn_client_protocols
+    address_space                 = var.vnetgw_vpn_client_configuration_address_space
+    vpn_client_protocols          = var.vnetgw_vpn_client_configuration_vpn_client_protocols
+
     root_certificate {
-      vnetgw_root_certificate_name                       = var.vnetgw_root_certificate_name
-      vnetgw_root_certificate_name_public_cert_data      = var.vnetgw_root_certificate_public_cert_data
+      name                        = var.vnetgw_root_certificate_name
+      public_cert_data            = var.vnetgw_root_certificate_public_cert_data
     }
+
   }
   
   bgp_settings {
-    vnetgw_bgp_settings_asn                              = var.vnetgw_bgp_settings_asn
-    vnetgw_bgp_settings_peering_addresses                = var.vnetgw_bgp_settings_peering_addresses
-    vnetgw_bgp_settings_peer_weight                      = var.vnetgw_bgp_settings_peer_weight
+    asn                           = var.vnetgw_bgp_settings_asn
+    peering_addresses             = var.vnetgw_bgp_settings_peering_addresses
+    peer_weight                   = var.vnetgw_bgp_settings_peer_weight
   }
+
 }
 
 output "vnetgw_id" {
--- vnetgw/vnetgw.var.tf
+++ vnetgw/vnetgw.var.tf
@@ -69,7 +69,10 @@ variable "vnetgw_vpn_client_configuration_vpn_client_protocols" {
         "vpn_client_protocols"
     ]
 
-variable "vnetgw_root_certificate_name"{}
+variable "vnetgw_root_certificate_name"{
+    type = string
+    default = "vnetgw_root_certificate_name"
+}
 variable "vnetgw_root_certificate_public_cert_data"{}
 variable "vnetgw_bgp_settings_asn"{}
 variable "vnetgw_bgp_settings_peering_addresses"{}
